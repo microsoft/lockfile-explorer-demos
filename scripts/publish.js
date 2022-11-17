410f0774934d791c8e183ff7b996d326b245a3e5
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2022-11-17 13:51:59 -0800

    Fix verdaccio login

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,52 +1,87 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 
+const NpmRegistryClient = require('npm-registry-client');
+
 // DO NOT STORE PASSWORDS IN SOURCE CODE
 // This password is saved here because Verdaccio runs on localhost
 // and is used for testing purposes only, so there is no security concern.
-const NPM_TOKEN = `//localhost:54321/:_authToken="BUocaxb4NA8iQuWjGBZHcw=="`;
+const username = 'test-only';
+const password = 'password';
+const email = 'test-only@example.com';
 
-const REGISTRY_PARAMETER = `--registry=http://localhost:54321`;
+const verdaccioUrl = 'http://localhost:54321';
+const verdaccioShortUrl = '//localhost:54321';
 
-const repoRootPath = path.resolve(__dirname, '..');
-const demoFolderPath = path.join(repoRootPath, 'demo-packages');
+function publishPackages(npmAccessToken) {
+  const registryCliParameter = `--registry=${verdaccioUrl}`;
+  const authCliParameter = `--${verdaccioShortUrl}/:_authToken="${npmAccessToken}"`
 
-const folderNames = fs.readdirSync(demoFolderPath);
+  const repoRootPath = path.resolve(__dirname, '..');
+  const demoFolderPath = path.join(repoRootPath, 'demo-packages');
 
-for (folderName of folderNames) {
-  if (folderName.indexOf('@') < 0) {
-    continue;
-  }
+  const folderNames = fs.readdirSync(demoFolderPath);
 
-  const fullPath = path.join(demoFolderPath, folderName);
+  for (const folderName of folderNames) {
+    if (folderName.indexOf('@') < 0) {
+      continue;
+    }
 
-  console.log(`\n*** Processing ${fullPath}`);
+    const fullPath = path.join(demoFolderPath, folderName);
 
-  const packageJson = require(path.join(fullPath, 'package.json'));
+    console.log(`\n*** Processing ${fullPath}`);
 
-  console.log(`*** Checking ${packageJson.name}@${packageJson.version}`);
+    const packageJson = require(path.join(fullPath, 'package.json'));
 
-  let outputObject = {};
-  try {
-    const outputJson = child_process
-      .execSync(`pnpm view ${packageJson.name} time ${REGISTRY_PARAMETER} --json`, {
-        stdio: ['ignore', 'pipe', 'ignore'],
+    console.log(`*** Checking ${packageJson.name}@${packageJson.version}`);
+
+    let outputObject = {};
+    try {
+      const outputJson = child_process
+        .execSync(`pnpm view ${packageJson.name} time ${registryCliParameter} --json`, {
+          stdio: ['ignore', 'pipe', 'ignore'],
+          cwd: fullPath
+        })
+        .toString();
+      outputObject = JSON.parse(outputJson);
+    } catch (e) {
+      console.log('Package has not been published yet.');
+    }
+
+    if (outputObject[packageJson.version]) {
+      console.log(`${packageJson.name}@${packageJson.version} is already published.`);
+    } else {
+      console.log(`*** ${packageJson.name}@${packageJson.version} publishing...`);
+      child_process.execSync(`pnpm publish --no-git-checks ${authCliParameter}`, {
+        stdio: 'inherit',
         cwd: fullPath
-      })
-      .toString();
-    outputObject = JSON.parse(outputJson);
-  } catch (e) {
-    console.log('Package has not been published yet.');
-  }
-
-  if (outputObject[packageJson.version]) {
-    console.log(`${packageJson.name}@${packageJson.version} is already published.`);
-  } else {
-    console.log(`*** ${packageJson.name}@${packageJson.version} publishing...`);
-    child_process.execSync(`pnpm publish --no-git-checks --${NPM_TOKEN}`, {
-      stdio: 'inherit',
-      cwd: fullPath
-    });
+      });
+    }
   }
 }
+
+const adduserParams = {
+  auth: {
+    username,
+    password,
+    email
+  },
+  timeout: 10000
+};
+
+const client = new NpmRegistryClient();
+
+client.adduser(verdaccioUrl, adduserParams, function (error, data, raw, res) {
+  if (error) {
+    console.error('NPM login failed:');
+    console.error(error);
+    return;
+  }
+  console.log(`NPM login succeeded`);
+  const npmAccessToken = data.token;
+
+  publishPackages(npmAccessToken);
+});
